69f07195722d3ec74eedf54afef8f26067121bae
haywirephoenix <andy_reed1@live.co.uk>
2024-04-18 23:35:11 +0100

    Fix input module errors

--- Editor/Modules/SOMInputModule.cs
+++ Editor/Modules/SOMInputModule.cs
@@ -1,44 +1,61 @@
 ﻿using UnityEditor;
 using System.Collections.Generic;
 
-namespace SOM{
+namespace SOM
+{
 	/// <summary>
 	/// The Input Module adds a constant for every axes name in the ProjectSettings/InputManager.asset file
 	/// </summary>
-	public class SOMInputModule:SOMModule{
+	public class SOMInputModule : SOMModule
+	{
 		//===================================
 		//Properties
 		//===================================
-		public override string moduleName{
-			get{return "Input";}
+		public override string moduleName
+		{
+			get { return "InputConsts"; }
 		}
 
 		//===================================
 		//Refresh
 		//===================================
-		public override void Refresh(){
+		public override void Refresh()
+		{
 			string[] values = GetInputAxis().ToArray();
 			string[] inputs = new string[values.Length];
 
 			for (int i = 0; i < inputs.Length; i++)
 				inputs[i] = SOMUtils.NicifyConstantName(values[i]);
-			SOMXmlHandler.AddConstants(moduleName, inputs, values);
+
+			SOMDataHandler.AddConstants(moduleName, inputs, values);
 		}
 
-		//This returns a list with all axes names
-		//Borrowed from http://answers.unity3d.com/questions/21083/how-do-you-get-the-button-names-assigned-to-an-axi.html
-		public static List<string> GetInputAxis(){
+
+		public static List<string> GetInputAxis()
+		{
 			List<string> allAxis = new List<string>();
 			SerializedObject serializedObject = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/InputManager.asset")[0]);
 			SerializedProperty axesProperty = serializedObject.FindProperty("m_Axes");
-			axesProperty.Next(true);
-			axesProperty.Next(true);
-			while (axesProperty.Next(false)){
-				SerializedProperty axis = axesProperty.Copy();
-				axis.Next(true);
-				if (!allAxis.Contains(axis.stringValue))
-					allAxis.Add(axis.stringValue);
+
+			// Ensure the axes property is valid
+			if (axesProperty != null && axesProperty.isArray)
+			{
+				// Iterate over each element in the array
+				for (int i = 0; i < axesProperty.arraySize; i++)
+				{
+					SerializedProperty axis = axesProperty.GetArrayElementAtIndex(i);
+
+					// Get the "name" property of the axis
+					SerializedProperty nameProperty = axis.FindPropertyRelative("m_Name");
+
+					// Ensure the "name" property is valid and add it to the list
+					if (nameProperty != null && !string.IsNullOrEmpty(nameProperty.stringValue))
+					{
+						allAxis.Add(nameProperty.stringValue);
+					}
+				}
 			}
+
 			return allAxis;
 		}
 	}
