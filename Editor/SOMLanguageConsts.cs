694dd49c153eda63240bdc0a525281c8c78e3243
haywirephoenix <andy_reed1@live.co.uk>
2024-04-29 10:43:25 +0100

    Add global language consts

---
+++ Editor/SOMLanguageConsts.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SOM
+{
+    public static class LanguageConsts
+    {
+
+        public const string _public = "public";
+        public const string _namespace = "namespace";
+        public const string _static = "static";
+        public const string _const = "const";
+        public const string _class = "class";
+        public const string _struct = "struct";
+        public const string _string = "string";
+        public const string _int = "int";
+        public const string _openBrace = "{";
+        public const string _closeBrace = "}";
+        public const string _doublefslash = "//";
+        public const string _blockQuoteOpen = "/*";
+        public const string _blockQuoteClose = "*/";
+        public const Char _dotChar = '.';
+        public const Char _plusChar = '+';
+        public const Char _indentChar = '\t';
+        public const string _indentStr = "\t";
+        public const string _dotStr = ".";
+        public const string _dotCS = ".cs";
+        public const string _dotTxt = ".txt";
+        public static readonly string _newLine = Environment.NewLine;
+
+    }
+
+
+}
