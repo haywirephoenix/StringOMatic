40ed0306436d1ae8dc2b584cd9327c22dae42734
haywirephoenix <andy_reed1@live.co.uk>
2024-05-03 12:22:23 +0100

    defines rework

--- Editor/SOMPackageChecker.cs
+++ Editor/SOMPackageChecker.cs
@@ -3,16 +3,22 @@ using UnityEditor;
 using UnityEditor.PackageManager;
 namespace SOM
 {
+
+    [InitializeOnLoad]
     public class SOMPackageChecker : AssetPostprocessor
     {
 
-        protected void OnPreprocessAsset()
+        static SOMPackageChecker()
         {
+            AssetDatabase.importPackageStarted += OnImportPackageStarted;
+        }
 
+        private static void OnImportPackageStarted(string packagename)
+        {
             SOMAddressableCheck.CheckForAddressables();
             SOMRewiredCheck.CheckForRewired();
-
         }
+
 
     }
 
